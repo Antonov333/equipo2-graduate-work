c180921af587ce0ae1d62e2c7f6d14266fa736ce
Antonov333 <antonovsergei333@yahoo.com>
2024-01-09 15:37:56 +0300

    Fixed showing avatars with UI

--- src/main/java/ru/skypro/homework/controller/ImagesController.java
+++ src/main/java/ru/skypro/homework/controller/ImagesController.java
@@ -3,15 +3,13 @@ package ru.skypro.homework.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Data;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.service.ImagesService;
 
 @RestController
 @Data
 @Tag(name = "Images Controller", description = "Picture delivery to frontend application")
+@CrossOrigin(value = "http://localhost:3000")
 @RequestMapping("/images")
 public class ImagesController {
     private final ImagesService imagesService;
