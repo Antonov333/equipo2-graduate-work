98f4f968f2b6d8878d2e3d35a6859cb5baa2ae39
Antonov333 <antonovsergei333@yahoo.com>
2024-01-09 15:21:34 +0300

    Changed methods changeAvatar in AvatarController and setAvatar in UserServiceImpl

--- src/main/java/ru/skypro/homework/controller/AvatarController.java
+++ src/main/java/ru/skypro/homework/controller/AvatarController.java
@@ -2,10 +2,11 @@ package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,11 +23,13 @@ import java.security.Principal;
 @RequiredArgsConstructor
 public class AvatarController {
     private final UserServiceImpl userService;
+    Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
-    @PostMapping(value = "/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @PreAuthorize("")
+    @PostMapping(value = "users/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
     public ResponseEntity<HttpStatus> changeAvatar(@RequestBody MultipartFile avatarSourceFile,
                                                    Principal principal) throws IOException {
+        logger.info("changeAvatar | user login name: " + principal.getName());
         return userService.setAvatar(avatarSourceFile, principal);
     }
 }
