08e0968c10b3931d21eeb2ebdd6ae3e4a7acce87
Antonov333 <antonovsergei333@yahoo.com>
2024-01-09 17:34:12 +0300

    Made sure item pictures delivered to frontend

--- src/main/java/ru/skypro/homework/mapping/AdMapper.java
+++ src/main/java/ru/skypro/homework/mapping/AdMapper.java
@@ -9,7 +9,28 @@ import ru.skypro.homework.model.Ad;
 @Mapper
 public interface AdMapper {
     AdMapper INSTANCE = Mappers.getMapper(AdMapper.class);
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id")
+//    private Long pk;
+//    @Column(name = "Author")
+//    private Integer author;
+
+
+    //
+//    @Column(name = "URL")
+//    private String image;
+//
+//    @Column(name = "Price")
+//    @Size(min = 0, max = 1000000000)
+//    private Integer price;
+//
+//    @Column(name = "Title")
+//    @Size(min = 4, max = 32)
+//    private String title;
     AdDto adToDto (Ad ad);
+
 
     Ad adDtoToad(AdDto dto);
 
